10a64561edfbb036dd8bcc11fc882a581bbd1acc
Terry Horowitz <terrychorowitz@gmail.com>
2016-03-20 18:29:42 -0400

    added boards

---
+++ seed/boards.js
@@ -0,0 +1,86 @@
+module.exports = [
+  {
+    name: "Alexandria",
+    picture: "b_alexandria.png",
+    resource: "glass",
+    wonder1: [3],
+    wonder1Cost: ["stone", "stone"],
+    wonder2: ["Raw Resource"],
+    wonder2Cost: ["ore", "ore"],
+    wonder3: [7],
+    wonder3Cost: ["glass", "glass"],
+    side: "A"
+  },
+  {
+    name: "Babylon",
+    picture: "b_babylon.png",
+    resource: "clay",
+    wonder1: [3],
+    wonder1Cost: ["clay", "clay"],
+    wonder2: ["Technology"],
+    wonder2Cost: ["wood", "wood", "wood"],
+    wonder3: [7],
+    wonder3Cost: ["clay", "clay", "clay", "clay"],
+    side: "A"
+  },
+  {
+    name: "Ephesos",
+    picture: "b_ephesos.png",
+    resource: "papyrus",
+    wonder1: [3],
+    wonder1Cost: ["stone", "stone"],
+    wonder2: [9, "gold"],
+    wonder2Cost: ["wood", "wood"],
+    wonder3: [7],
+    wonder3Cost: ["papyrus", "papyrus"],
+    side: "A"
+  },
+  {
+    name: "Gizah",
+    picture: "b_gizah.png",
+    resource: "stone",
+    wonder1: [3],
+    wonder1Cost: ["stone", "stone"],
+    wonder2: [5],
+    wonder2Cost: ["wood", "wood", "wood"],
+    wonder3: [7],
+    wonder3Cost: ["stone", "stone", "stone", "stone"],
+    side: "A"
+  },
+  {
+    name: "Halikarnassos",
+    picture: "b_halikarnassos.png",
+    resource: "textile",
+    wonder1: [3],
+    wonder1Cost: ["clay", "clay"],
+    wonder2: ["Discard"],
+    wonder2Cost: ["ore", "ore", "ore"],
+    wonder3: [7],
+    wonder3Cost: ["textile", "textile"],
+    side: "A"
+  },
+  {
+    name: "Olympia",
+    picture: "b_Olympia.png",
+    resource: "wood",
+    wonder1: [3],
+    wonder1Cost: ["wood", "wood"],
+    wonder2: ["Free Cards"],
+    wonder2Cost: ["stone", "stone"],
+    wonder3: [7],
+    wonder3Cost: ["ore", "ore"],
+    side: "A"
+  },
+  {
+    name: "Rhodos",
+    picture: "b_rhodos.png",
+    resource: "ore",
+    wonder1: [3],
+    wonder1Cost: ["wood", "wood"],
+    wonder2: ["war", "war"],
+    wonder2Cost: ["clay", "clay", "clay"],
+    wonder3: [7],
+    wonder3Cost: ["ore", "ore", "ore", "ore"],
+    side: "A"
+  }
+]
