654e81f781c9c352963f9b1b443de6f71d146ca3
Terry Horowitz <terrychorowitz@gmail.com>
2016-03-20 19:10:46 -0400

    routes for testing models added

--- server/app/routes/cards/index.js
+++ server/app/routes/cards/index.js
@@ -1,28 +1,48 @@
 'use strict';
 var router = require('express').Router();
 var Board = require('../../../db/models/index').Board;
+var Player = require('../../../db/models/index').Player;
+var Deck = require('../../../db/models/index').Deck;
+var Game = require('../../../db/models/index').Game;
 var Card = require('../../../db/models/index').Card;
 module.exports = router;
 
-router.get('/', function(req, res, next){
+router.get('/board', function(req, res, next){
   Board.findAll()
-  .then(function(cards){
-    console.log('HOW MANY', cards.length)
-    res.json(cards)
+  .then(function(boards){
+    console.log('HOW MANY', boards.length)
+    res.json(boards)
   }).catch(next)
 });
 
-router.get('/some', function(req, res, next){
-  Card.find({id: 1})
-  .then(function(cards){
-    console.log('HOW MANY', cards.length)
-    res.json(cards)
+router.get('/player', function(req, res, next){
+  Player.findAll()
+  .then(function(players){
+    console.log('HOW MANY', players.length)
+    res.json(players)
   }).catch(next)
 });
 
-router.post('/', function(req, res, next){
-  Card.create(req.body)
-  .then(function(response){
-    res.json(response.data)
+router.get('/deck', function(req, res, next){
+  Deck.findAll()
+  .then(function(deck){
+    console.log('HOW MANY', deck.length)
+    res.json(deck)
+  }).catch(next)
+});
+
+router.get('/game', function(req, res, next){
+  Game.findAll()
+  .then(function(games){
+    console.log('HOW MANY', games.length)
+    res.json(games)
+  }).catch(next)
+});
+
+router.get('/card', function(req, res, next){
+  Card.findAll()
+  .then(function(cards){
+    console.log('HOW MANY', cards.length)
+    res.json(cards)
   }).catch(next)
 });
