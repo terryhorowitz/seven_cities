b6fbcca89108ae0a4c87c571938e7d75d11b0354
Terry Horowitz <terrychorowitz@gmail.com>
2016-03-20 18:15:06 -0400

    added wonders to board

--- server/db/models/board.js
+++ server/db/models/board.js
@@ -6,7 +6,14 @@ module.exports = function(db) {
   var Board = db.define('board', {
     name: Sequelize.STRING,
     picture: Sequelize.STRING,
-    resource: Sequelize.STRING
+    resource: Sequelize.STRING,
+    wonder1: Sequelize.ARRAY,
+    wonder1Cost: Sequelize.ARRAY,
+    wonder2: Sequelize.ARRAY,
+    wonder2Cost: Sequelize.ARRAY,
+    wonder3: Sequelize.ARRAY,
+    wonder3Cost: Sequelize.ARRAY,
+    side: Sequelize.INTEGER
   }, {
     timestamps: false
   });
