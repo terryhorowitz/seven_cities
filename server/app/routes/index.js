05d645f8e4e7c17a36261e6325341077f57bca86
Terry Horowitz <terrychorowitz@gmail.com>
2016-03-21 09:20:56 -0400

    removed cards route folder

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -3,7 +3,7 @@ var router = require('express').Router();
 module.exports = router;
 
 //router.use('/members', require('./members'));
-router.use('/test', require('./cards'));
+router.use('/test', require('./routes'));
 
 // Make sure this is after all of
 // the registered routes!
