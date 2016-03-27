6d4623ea4d07e701f1c004f4bf8ccb36331d246b
leilabl <leilaloezer@gmail.com>
2016-03-27 14:42:20 -0400

    add expand pile function to neighbor view

--- browser/js/other-players/other-players.js
+++ browser/js/other-players/other-players.js
@@ -28,6 +28,10 @@ app.directive('otherPlayers', function ($rootScope, $state, $uibModal) {
                 scope.background = {'background-color': 'blue', 'background': 'url(img/background/' + scope.neighbor.board.name + '.png)'}
 
                 // $scope.background = {background: 'url(img/background/' + $scope.me.board.name + '.png) no-repeat center center fixed', 'background-size': 'cover', 'min-height': '100%'};
+                scope.expandPile = function (pile) {
+                    if (!scope.clickedPile) scope.clickedPile = pile;
+                    else scope.clickedPile = false;
+                  }
 
             }
 
