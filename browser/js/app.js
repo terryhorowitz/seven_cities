46b7c24edfc99e23643396fd946359257595f07b
Yuval Idan <yuval.idan5@gmail.com>
2016-03-30 12:00:17 -0400

    commented out a bunch of unneeded files

--- browser/js/app.js
+++ browser/js/app.js
@@ -1,5 +1,5 @@
 'use strict';
-window.app = angular.module('FullstackGeneratedApp', ['fsaPreBuilt', 'ui.router', 'ui.bootstrap', 'ngAnimate'])
+window.app = angular.module('FullstackGeneratedApp', ['ui.router', 'ui.bootstrap', 'ngAnimate'])
 // .factory('mySocket', function(socketFactory){
 //     return socketFactory();
 // });
@@ -11,44 +11,44 @@ app.config(function ($urlRouterProvider, $locationProvider) {
     $urlRouterProvider.otherwise('/');
 });
 
-// This app.run is for controlling access to specific states.
-app.run(function ($rootScope, AuthService, $state) {
+// // This app.run is for controlling access to specific states.
+// app.run(function ($rootScope, $state) {
 
-    // The given state requires an authenticated user.
-    var destinationStateRequiresAuth = function (state) {
-        return state.data && state.data.authenticate;
-    };
+//     // The given state requires an authenticated user.
+//     var destinationStateRequiresAuth = function (state) {
+//         return state.data && state.data.authenticate;
+//     };
 
-    // $stateChangeStart is an event fired
-    // whenever the process of changing a state begins.
-    $rootScope.$on('$stateChangeStart', function (event, toState, toParams) {
+//     // $stateChangeStart is an event fired
+//     // whenever the process of changing a state begins.
+//     $rootScope.$on('$stateChangeStart', function (event, toState, toParams) {
 
-        if (!destinationStateRequiresAuth(toState)) {
-            // The destination state does not require authentication
-            // Short circuit with return.
-            return;
-        }
+//         // if (!destinationStateRequiresAuth(toState)) {
+//         //     // The destination state does not require authentication
+//         //     // Short circuit with return.
+//         //     return;
+//         // }
 
-        if (AuthService.isAuthenticated()) {
-            // The user is authenticated.
-            // Short circuit with return.
-            return;
-        }
+//         // if (AuthService.isAuthenticated()) {
+//         //     // The user is authenticated.
+//         //     // Short circuit with return.
+//         //     return;
+//         // }
 
-        // Cancel navigating to new state.
-        event.preventDefault();
+//         // Cancel navigating to new state.
+//         event.preventDefault();
 
-        AuthService.getLoggedInUser().then(function (user) {
-            // If a user is retrieved, then renavigate to the destination
-            // (the second time, AuthService.isAuthenticated() will work)
-            // otherwise, if no user is logged in, go to "login" state.
-            if (user) {
-                $state.go(toState.name, toParams);
-            } else {
-                $state.go('login');
-            }
-        });
+//         AuthService.getLoggedInUser().then(function (user) {
+//             // If a user is retrieved, then renavigate to the destination
+//             // (the second time, AuthService.isAuthenticated() will work)
+//             // otherwise, if no user is logged in, go to "login" state.
+//             if (user) {
+//                 $state.go(toState.name, toParams);
+//             } else {
+//                 $state.go('login');
+//             }
+//         });
 
-    });
+//     });
 
-});
+// });
