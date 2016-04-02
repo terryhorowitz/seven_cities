1d77553622a20c4b563ff3b9abb7ee2f8381dc01
Yuval Idan <yuval.idan5@gmail.com>
2016-04-02 16:28:12 -0400

    included guilds in the deck finally

--- browser/js/game/game.js
+++ browser/js/game/game.js
@@ -134,7 +134,7 @@ app.controller('GameController', function ($scope, $state, TradeFactory) {
         $scope.submitChoice = function(selection){
           $scope.submitted = true;
           if (selection === "Build (free once per era)") {
-            $scope.alreadyBuiltForFree[$scope.currentEra = false];
+            $scope.alreadyBuiltForFree[$scope.currentEra] = false;
             selection = 'Build for free';
           }
           socket.emit('submit choice', {choice: selection, cardId: $scope.cardSelection.id, playerId: $scope.playerId});
@@ -377,7 +377,7 @@ app.controller('GameController', function ($scope, $state, TradeFactory) {
       })
 
       $scope.set_wonder = function(wonder) {
-        if (wonder <= $scope.me.wondersBuilt) {
+        if ($scope.me && wonder <= $scope.me.wondersBuilt) {
           return {
             '-webkit-filter' : 'grayscale(100%)',
             'filter' : 'grayscale(100%)'  
