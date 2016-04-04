61c228d55ce211b703dd4f9856a19c951a2fdda3
Terry Horowitz <terrychorowitz@gmail.com>
2016-04-03 23:06:47 -0400

    updated trade validation

--- browser/js/game/trade_factory.js
+++ browser/js/game/trade_factory.js
@@ -6,6 +6,7 @@ app.factory('TradeFactory', function(){
   TradeFactory.isValidTrade = function (tradeInput, total) {
     var combinedTrade = [].slice.call(tradeInput.left).concat([].slice.call(tradeInput.right)).concat([].slice.call(tradeInput.self)).sort(function(a,b){ return a > b });
     var totalCopy = [].slice.call(total).sort(function(a,b){ return a > b });
+    if (combinedTrade.length !== totalCopy.length) return false;
     for (var i = 0; i < totalCopy.length; i++){
     	if (combinedTrade[i] !== totalCopy[i]) return false;
     }
