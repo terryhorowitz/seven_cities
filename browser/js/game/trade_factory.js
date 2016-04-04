8965f6e87bb7009bb717c89fdc23845b8407b8f5
Terry Horowitz <terrychorowitz@gmail.com>
2016-04-04 09:02:03 -0400

    message for watching tade

--- browser/js/game/trade_factory.js
+++ browser/js/game/trade_factory.js
@@ -4,6 +4,7 @@ app.factory('TradeFactory', function(){
   var TradeFactory = {};
   
   TradeFactory.isValidTrade = function (tradeInput, total) {
+    console.log('trades', tradeInput, total)
     var combinedTrade = [].slice.call(tradeInput.left).concat([].slice.call(tradeInput.right)).concat([].slice.call(tradeInput.self)).sort(function(a,b){ return a > b });
     var totalCopy = [].slice.call(total).sort(function(a,b){ return a > b });
     if (combinedTrade.length !== totalCopy.length) return false;
